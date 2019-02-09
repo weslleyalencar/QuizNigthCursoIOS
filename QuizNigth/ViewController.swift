99fde1f9815546f76dd21500dbc23e9071ca8845
Weslley Alencar <weslley_hssilva@hotmail.com>
2019-02-08 23:05:48 -0200

    Initial Commit

---
+++ QuizNigth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizNigth
+//
+//  Created by Weslley Alencar on 08/02/19.
+//  Copyright © 2019 Business Office. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
